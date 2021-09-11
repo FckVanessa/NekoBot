2eed89c69a78ff3447c99a934c83347904e39fa5
FckVanessa <90359820+FckVanessa@users.noreply.github.com>
2021-09-11 19:08:43 +0700

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -1,7 +1,7 @@
 class Config(object):
     LOGGER = True
     TOKEN = "1965069129:AAFO3Om7xiK_BoUHJluELv4vZNY2Xw6PYHQ"
-    MONGO_URI = "mongodb+srv://username:<password>@cluster0.c11xf.mongodb.net/<dbname>?retryWrites=true&w=majority"
+    MONGO_URI = "mongodb+srv://vira:MafGans@12@cluster0.qzouh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 
 class Production(Config):
